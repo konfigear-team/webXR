75ee86b7eedce762f3be20043c28677bebab3207
Ait Oufkir Yassine <contact@konfigear.com>
2024-08-17 23:19:54 +0200

    added 3D Models to scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,56 @@
-import { Canvas, createPortal, useThree } from "@react-three/fiber";
-import { createXRStore, XR, XRDomOverlay, XROrigin } from "@react-three/xr";
-import { Suspense, useState } from "react";
-import { Box } from "@react-three/drei";
+import { Center, Environment, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { XR, XROrigin, createXRStore } from "@react-three/xr";
+import { Suspense } from "react";
+import { Model } from "./Datsun.jsx";
 
-const store = createXRStore();
+const store = createXRStore({ depthSensing: true, hand: false });
 
 export function App() {
-  const [bool, setBool] = useState(false);
   return (
     <>
-      <button onClick={() => store.enterVR()}>Enter VR</button>
-      <button onClick={() => store.enterAR()}>Enter AR</button>
-      <Canvas style={{ width: "100vw", flexGrow: 1, height: "100vh" }}>
+      <button
+        style={{
+          position: "absolute",
+          zIndex: 10000,
+          background: "black",
+          borderRadius: "0.5rem",
+          border: "none",
+          fontWeight: "bold",
+          color: "white",
+          padding: "1rem 2rem",
+          cursor: "pointer",
+          fontSize: "1.5rem",
+          bottom: "1rem",
+          left: "50%",
+          boxShadow: "0px 0px 20px rgba(0,0,0,1)",
+          transform: "translate(-50%, 0)",
+        }}
+        onClick={() => store.enterAR()}
+      >
+        Enter AR
+      </button>
+      <Canvas style={{ width: "100vw", height: "100vh" }} shadows camera={{ position: [4, 0, 6], fov: 35 }}>
         <XR store={store}>
-          <XROrigin />
-
-          <XRDomOverlay style={{ width: "100%", height: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
-            <div style={{ backgroundColor: bool ? "red" : "green", padding: "1rem 2rem" }} onClick={() => setBool((b) => !b)}>
-              Hello World
-            </div>
-          </XRDomOverlay>
-          <Suspense fallback={null}>
-            <Box position={[0, 0, 0]} scale={3} />
+          <group position={[0, -0.75, 0]}>
+            <Suspense>
+              <Center top>
+                <Model />
+              </Center>
+            </Suspense>
+            <directionalLight position={[1, 8, 1]} castShadow />
+            <ambientLight />
+            <mesh receiveShadow rotation-x={-Math.PI / 2} scale={100}>
+              <shadowMaterial opacity={0.7} />
+              <planeGeometry />
+            </mesh>
+            <group position={[0, 0, 2.6]}>
+              <XROrigin />
+            </group>
+          </group>
+          <OrbitControls />
+          <Suspense>
+            <Environment preset="dawn" blur={1} />
           </Suspense>
         </XR>
       </Canvas>
