e61e2844c7b228c650a2ede77988b5664ff0f33c
Ait Oufkir Yassine <contact@konfigear.com>
2024-08-18 00:24:13 +0200

    hit test v3

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,140 @@
-import { Center, Environment, OrbitControls } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { XR, XRHitTest, XROrigin, createXRStore } from "@react-three/xr";
-import { Suspense, useRef } from "react";
-import { Model } from "./Datsun.jsx";
-import { Matrix4, Vector3 } from "three";
+import { Canvas } from '@react-three/fiber'
+import {
+  XRSpace,
+  createXRStore,
+  XR,
+  XRHitTest,
+  XRControllerModel,
+  XRHandModel,
+  GetWorldMatrixFromXRHitTest,
+  useXRInputSourceEvent,
+  useXRRequestHitTest,
+  useXRAnchor,
+  useXRInputSourceStateContext,
+  useXRInputSourceState,
+} from '@react-three/xr'
+import { useEffect, useRef } from 'react'
+import { Matrix4, Mesh, Vector3 } from 'three'
+import { create } from 'zustand'
 
-const matrixHelper = new Matrix4();
-const hitTestPosition = new Vector3();
+const matrixHelper = new Matrix4()
 
-const store = createXRStore({
-  hand: {
-    right: () => {
-      const state = useXRHandState();
-      return (
-        <>
-          <XRHitTest
-            space={state.inputSource.targetRaySpace}
-            onResults={(results, getWorldMatrix) => {
-              console.log(results);
-
-              if (results.length === 0) {
-                return;
-              }
-              getWorldMatrix(matrixHelper, results[0]);
-              hitTestPosition.setFromMatrixPosition(matrixHelper);
-            }}
-          />
-        </>
-      );
-    },
-  },
-});
-
-function Point() {
-  const ref = useRef();
-  useFrame(() => ref.current?.position.copy(hitTestPosition));
-  return (
-    <group ref={ref}>
-      <Model />
-    </group>
-  );
+function onResults(handedness: XRHandedness, results: XRHitTestResult[], getWorldMatrix: GetWorldMatrixFromXRHitTest) {
+  ;(handedness === 'left' ? useLeftPoints : useRightPoints).setState(
+    results
+      .map((result) => {
+        if (!getWorldMatrix(matrixHelper, result)) {
+          return undefined
+        }
+        return new Vector3().setFromMatrixPosition(matrixHelper)
+      })
+      .filter((vector) => vector != null),
+  )
 }
 
+const store = createXRStore({
+  hand: () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const state = useXRInputSourceStateContext()
+    return (
+      <>
+        <XRHandModel />
+        <XRSpace space={state.inputSource.targetRaySpace}>
+          <XRHitTest onResults={onResults.bind(null, state.inputSource.handedness)} />
+        </XRSpace>
+      </>
+    )
+  },
+  controller: () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const state = useXRInputSourceStateContext()
+    return (
+      <>
+        <XRControllerModel />
+        <XRHitTest
+          space={state.inputSource.targetRaySpace}
+          onResults={onResults.bind(null, state.inputSource.handedness)}
+        />
+      </>
+    )
+  },
+  screenInput: () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const state = useXRInputSourceStateContext()
+    return (
+      <XRHitTest
+        space={state.inputSource.targetRaySpace}
+        onResults={onResults.bind(null, state.inputSource.handedness)}
+      />
+    )
+  },
+})
+
+const useLeftPoints = create<Array<Vector3>>(() => [])
+const useRightPoints = create<Array<Vector3>>(() => [])
+
 export function App() {
+  const leftLength = useLeftPoints((s) => s.length)
+  const rightLength = useRightPoints((s) => s.length)
   return (
     <>
-      <button
-        style={{
-          position: "absolute",
-          zIndex: 10000,
-          background: "black",
-          borderRadius: "0.5rem",
-          border: "none",
-          fontWeight: "bold",
-          color: "white",
-          padding: "1rem 2rem",
-          cursor: "pointer",
-          fontSize: "1.5rem",
-          bottom: "1rem",
-          left: "50%",
-          boxShadow: "0px 0px 20px rgba(0,0,0,1)",
-          transform: "translate(-50%, 0)",
-        }}
-        onClick={() => store.enterAR()}
-      >
-        Enter AR
-      </button>
-      <Canvas style={{ width: "100vw", height: "100vh" }} shadows camera={{ position: [4, 0, 6], fov: 35 }}>
+      <button onClick={() => store.enterAR()}>Enter AR</button>
+      <Canvas style={{ width: '100%', flexGrow: 1 }}>
         <XR store={store}>
-          <group position={[0, -0.75, 0]}>
-            <Suspense>
-              <Center top>
-                <Point />
-              </Center>
-            </Suspense>
-
-            <group position={[0, 0, 2.6]}>
-              <XROrigin />
-            </group>
-          </group>
-          <OrbitControls />
-          <Suspense>
-            <Environment preset="dawn" blur={1} />
-          </Suspense>
+          <ambientLight />
+          {new Array(leftLength).fill(undefined).map((_, i) => (
+            <Point left key={i} index={i} />
+          ))}
+          {new Array(rightLength).fill(undefined).map((_, i) => (
+            <Point key={i} index={i} />
+          ))}
+          <Anchors />
         </XR>
       </Canvas>
     </>
-  );
+  )
+}
+
+function Anchors() {
+  const [anchor, requestAnchor] = useXRAnchor()
+  const requestHitTest = useXRRequestHitTest()
+  useXRInputSourceEvent(
+    'all',
+    'select',
+    async (e) => {
+      const result = await requestHitTest(e.inputSource.targetRaySpace)
+      if (result == null || result.results.length === 0) {
+        return
+      }
+      requestAnchor({ relativeTo: 'hit-test-result', hitTestResult: result.results[0] })
+    },
+    [requestHitTest, requestAnchor],
+  )
+  if (anchor == null) {
+    return null
+  }
+  return (
+    <XRSpace space={anchor.anchorSpace}>
+      <mesh scale={0.1}>
+        <boxGeometry />
+      </mesh>
+    </XRSpace>
+  )
+}
+
+function Point({ index, left }: { left?: boolean; index: number }) {
+  const ref = useRef<Mesh>(null)
+  useEffect(
+    () =>
+      (left ? useLeftPoints : useRightPoints).subscribe((state) => {
+        ref.current!.position.copy(state[index])
+      }),
+    [index, left],
+  )
+  return (
+    <mesh scale={0.05} ref={ref}>
+      <sphereGeometry />
+      <meshBasicMaterial />
+    </mesh>
+  )
 }
